1797507b69429be46e6603e749df980e70c43c44
Brandon Van Horn <bmdvanhorn@icloud.com>
2026-03-21 01:16:32 -0700

    added more layout to page.tsx

--- components/navbarcomponent.tsx
+++ components/navbarcomponent.tsx
@@ -1,32 +1,47 @@
-'use client'
+"use client";
 
 import purpleusericon from "@/public/assets/purpleusericon.png";
-import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle, TextInput } from "flowbite-react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+  TextInput,
+} from "flowbite-react";
 import Link from "next/link";
 import Image from "next/image";
 
 export function NavbarComponent() {
   return (
-    <Navbar fluid rounded>
-      <NavbarBrand as={Link} href="https://flowbite-react.com">
+    <div>
+      <Navbar fluid rounded className="!bg-white border-b-gray-950 border-b-1">
+        <NavbarBrand as={Link} href="https://flowbite-react.com">
           <Image
-                loading="eager"
-                alt="contact manager logo"
-                src={purpleusericon}
-                style={{
-                  maxWidth: "5%",
-                  height: "auto",
-                }}
-              />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Contact Flow</span>
-        <div className="mt-1 ml-3 font-black font-bold">
-          Contact Manage
+            loading="eager"
+            alt="contact manager logo"
+            src={purpleusericon}
+            style={{
+              maxWidth: "5%",
+              height: "auto",
+            }}
+          />
+          <span className="self-center whitespace-nowrap text-xl font-semibold text-[#A45CFF]">
+            Contact Flow
+          </span>
+          <div className="mt-1 ml-3 font-black font-bold text-2xl text-black">
+            Contact Manager
+          </div>
+        </NavbarBrand>
+        <div>
+          <TextInput placeholder="search contacts..." className="rounded-lg" />
         </div>
-      </NavbarBrand>
-      <NavbarToggle />
-      <NavbarCollapse>
-        <TextInput placeholder="search contacts..." required />
-      </NavbarCollapse>
-    </Navbar>
+      </Navbar>
+
+      <div className="bg-white">
+        <h1 className="text-black font-bold">Add New Contact</h1>
+        <p className="text-gray-600 font-light">Fill in the details below to add a new contact to your list</p>
+      </div>
+    </div>
   );
 }
