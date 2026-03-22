625201bfab39bc34d17a454d89cb0b73a8f5eb27
Brandon Van Horn <bmdvanhorn@icloud.com>
2026-03-22 12:52:29 -0700

    only thing left is connect backend

--- components/navbarcomponent.tsx
+++ components/navbarcomponent.tsx
@@ -4,6 +4,7 @@ import purpleusericon from "@/public/assets/purpleusericon.png";
 import emailicon from "@/public/assets/emailicon.png";
 import phoneicon from "@/public/assets/phoneicon.png";
 import usericon from "@/public/assets/userimage.png";
+import notesicon from "@/public/assets/notesicon.png";
 import {
   Navbar,
   NavbarBrand,
@@ -41,9 +42,9 @@ export function NavbarComponent() {
         </div>
       </Navbar>
 
-      <div className="columns">
+      <div>
         <div className="bg-white border-4">
-          <h1 className="text-black font-bold">Add New Contact</h1>
+          <h1 className="text-black font-extrabold">Add New Contact</h1>
           <p className="text-gray-600 font-light">
             Fill in the details below to add a new contact to your list
           </p>
@@ -87,8 +88,164 @@ export function NavbarComponent() {
             + Add Contact
           </button>
         </div>
-        <div className="bg-white columns">
-          <h1 className="text-black font-bold">All Contacts</h1>
+        <div className="bg-white">
+          <div>
+            <h1 className="text-black font-extrabold border-4 border-white">
+              All Contacts
+            </h1>
+            <h1 className="text-black font-light text-end">5 Contacts</h1>
+          </div>
+          <div className="flex flex-row justify-around">
+            <div>
+              <p className="text-gray-600 font-light">Name</p>
+              <p className="text-gray-600 pt-4 font-bold">Alice Smith</p>
+              <p className="text-gray-600 pt-4 font-bold">Bob Johnson</p>
+              <p className="text-gray-600 pt-4 font-bold">Charlie Brown</p>
+              <p className="text-gray-600 pt-4 font-bold">Diana Miller</p>
+              <p className="text-gray-600 pt-4 font-bold">Eve Davis</p>
+            </div>
+            <div>
+              <p className="text-gray-600 font-light">Email</p>
+              <p className="text-gray-600 pt-4 font-bold">
+                alice.smith@example.com
+              </p>
+              <p className="text-gray-600 pt-4 font-bold">
+                bob.johnson@example.com
+              </p>
+              <p className="text-gray-600 pt-4 font-bold">
+                charlie.brown@example.com
+              </p>
+              <p className="text-gray-600 pt-4 font-bold">
+                diana.miller@example.com
+              </p>
+              <p className="text-gray-600 pt-4 font-bold">
+                eve.davis@example.com
+              </p>
+            </div>
+            <div>
+              <p className="text-gray-600 font-light">Phone</p>
+              <p className="text-gray-600 pt-4 font-bold">+1 (555) 101-2020</p>
+              <p className="text-gray-600 pt-4 font-bold">+1 (555) 303-4040</p>
+              <p className="text-gray-600 pt-4 font-bold">+1 (555) 505-6060</p>
+              <p className="text-gray-600 pt-4 font-bold">+1 (555) 707-8080</p>
+              <p className="text-gray-600 pt-4 font-bold">+1 (555) 909-1010</p>
+            </div>
+            <div>
+              <p className="text-gray-600 font-light">Actions</p>
+              <div className="flex-col justify-around">
+                <div>
+                  <Image
+                    loading="eager"
+                    alt="notesicon"
+                    src={notesicon}
+                    style={{
+                      maxWidth: "4%",
+                      height: "auto",
+                    }}
+                  />
+                  <div id="redSquare1">
+                    <div
+                      style={{
+                        width: "40px",
+                        height: "40px",
+                        backgroundColor: "red",
+                        borderRadius: "10px",
+                        margin: "20px",
+                      }}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <Image
+                    loading="eager"
+                    alt="notesicon"
+                    src={notesicon}
+                    style={{
+                      maxWidth: "4%",
+                      height: "auto",
+                    }}
+                  />
+                  <div id="redSquare1">
+                    <div
+                      style={{
+                        width: "40px",
+                        height: "40px",
+                        backgroundColor: "red",
+                        borderRadius: "10px",
+                        margin: "20px",
+                      }}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <Image
+                    loading="eager"
+                    alt="notesicon"
+                    src={notesicon}
+                    style={{
+                      maxWidth: "4%",
+                      height: "auto",
+                    }}
+                  />
+                  <div id="redSquare1">
+                    <div
+                      style={{
+                        width: "40px",
+                        height: "40px",
+                        backgroundColor: "red",
+                        borderRadius: "10px",
+                        margin: "20px",
+                      }}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <Image
+                    loading="eager"
+                    alt="notesicon"
+                    src={notesicon}
+                    style={{
+                      maxWidth: "4%",
+                      height: "auto",
+                    }}
+                  />
+                  <div id="redSquare1">
+                    <div
+                      style={{
+                        width: "40px",
+                        height: "40px",
+                        backgroundColor: "red",
+                        borderRadius: "10px",
+                        margin: "20px",
+                      }}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <Image
+                    loading="eager"
+                    alt="notesicon"
+                    src={notesicon}
+                    style={{
+                      maxWidth: "4%",
+                      height: "auto",
+                    }}
+                  />
+                  <div id="redSquare1">
+                    <div
+                      style={{
+                        width: "40px",
+                        height: "40px",
+                        backgroundColor: "red",
+                        borderRadius: "10px",
+                        margin: "20px",
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
